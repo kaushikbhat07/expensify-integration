2f85153f79093bb257456d11efc6a61de613c130
kaushikbhat07 <kaushik.bhat11@learner.manipal.edu>
2022-12-04 21:02:31 +0530

    Refactor

--- integration/src/main/java/com/expensify/integration/repository/ExpenseRepository.java
+++
@@ -1,8 +0,0 @@
-package com.expensify.integration.repository;
-
-import com.expensify.integration.models.Expense;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ExpenseRepository extends MongoRepository<Expense, Long> {
-    Expense findByTransactionId(long transactionId);
-}
